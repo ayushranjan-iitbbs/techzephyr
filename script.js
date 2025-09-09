d52d1392e28192c6b05dfe2a3ddb675bfcf71e06
Ayush Ranjan <codingwithayush10@gmail.com>
2025-09-10 04:40:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -41,5 +41,5 @@ hrElements.forEach(hr => {
 });
  
   window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("typing-text").textContent = "5 Socs 1 Summit";
+    document.getElementById("typing-text").textContent = "5 SOCs 1 SUMMIT ";
   });
